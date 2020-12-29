2514ab6612bae9c93c27d1bcb64dd676e20d580f
InnaBlack <innablack85@gmail.com>
2020-12-29 15:06:22 +0100

    Initial Commit

---
+++ SwiftUIApi/SwiftUIApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApiApp.swift
+//  SwiftUIApi
+//
+//  Created by  inna on 29/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
